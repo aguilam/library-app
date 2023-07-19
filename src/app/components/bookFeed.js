c77b1f6708c1c1f1c098d78adc0ed864ae6dd6b0
aguilam <pexfex@mail.ru>
2023-07-20 02:48:30 +0400

    First version of search by name

--- src/app/components/bookFeed.js
+++ src/app/components/bookFeed.js
@@ -1,20 +1,94 @@
 'use client'
 import "./css/bookFeed.css";
 import React, { useState, useEffect } from 'react';
-import { Input,Button, Card, Skeleton } from 'antd';
+import { Button, Card, Skeleton } from 'antd';
 import SearchPanel from "./searchPanel";
+import { Checkbox, Input, Menu, Radio } from 'antd';
+import { Slider } from 'antd';
+const { Search } = Input;
+const { SubMenu } = Menu;
+const RadioGroup = Radio.Group;
+const onSearch = (value) => console.log(value);
 const { Meta } = Card;
+
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+
+const items = [
+  getItem('Жанры', 'genres', null, [
+    getItem(<Checkbox>Фантастика</Checkbox>, '1'),
+    getItem(<Checkbox>Детектив</Checkbox>, '2'),
+    getItem(<Checkbox>Роман</Checkbox>, '3'),
+    getItem(<Checkbox>Фэнтези</Checkbox>, '4'),
+    getItem(<Checkbox>Приключения</Checkbox>, '5'),
+    getItem(<Checkbox>Ужасы</Checkbox>, '6'),
+    getItem(<Checkbox>Классика</Checkbox>, '7'),
+    getItem(<Checkbox>Исторический роман</Checkbox>, '8'),
+    getItem(<Checkbox>Научная литература</Checkbox>, '9'),
+    getItem(<Checkbox>Биография и автобиография</Checkbox>, '10')
+  ]),
+  getItem('Языки', 'Language', null, [
+    getItem(<Radio>Русский</Radio>, '11'),
+    getItem(<Radio>Английский</Radio>, '12'),
+    getItem(<Radio>Французский</Radio>, '13'),
+    getItem(<Radio>Испанский</Radio>, '14'),
+    getItem(<Radio>Немецкий</Radio>, '15'),
+    getItem(<Radio>Итальянский</Radio>, '16')
+  ]),
+  getItem('Страны', 'Country', null, [
+    getItem(<Radio>Россия</Radio>, '17'),
+    getItem(<Radio>Великобритания</Radio>, '18'),
+    getItem(<Radio>Франция</Radio>, '19'),
+    getItem(<Radio>США</Radio>, '20'),
+    getItem(<Radio>Германия</Radio>, '21'),
+    getItem(<Radio>Испания</Radio>, '22'),
+    getItem(<Radio>Италия</Radio>, '23')
+  ]),
+];
 export default function BookFeed() {
-  let search = 'Гарри Поттер';
+  const getMenuItems = items => {
+    return items.map(item => {
+      if (item.type === 'divider') {
+        return <Menu.Divider key={item.key} />;
+      } else if (item.children) {
+        return (
+          <SubMenu key={item.key} icon={item.icon} title={item.label}>
+            {item.children.map(childItem => (
+              <Menu.Item key={childItem.key}>{childItem.label}</Menu.Item>
+            ))}
+          </SubMenu>
+        );
+      } else {
+        return (
+          <Menu.Item key={item.key}>
+            <RadioGroup>{item.label}</RadioGroup>
+          </Menu.Item>
+        );
+      }
+    });
+  };
+  const [search, setSearch] = useState('Гарри Поттер');
   const [books, setBooks] = useState([]);
   const [schoolBooks, setSchoolBooks] = useState([]);
   const [schoolBooksIsbn, setSchoolBooksIsbn] = useState([]);
-
+  const newValue = (value) => {
+    setSearch(value);
+    console.log(value)
+    console.log(search)
+  }
   const bookAdd = (Identifiers) => {
     console.log('Добавлена книга:', Identifiers);
     setSchoolBooksIsbn(prevId => [...prevId, Identifiers]);
   };
 
+  
   useEffect(() => {
     const fetchBooks = async () => {
       try {
@@ -66,7 +140,15 @@ export default function BookFeed() {
   return (
     <div className="bookRow">
       <div>
-        <SearchPanel></SearchPanel>
+        <Search placeholder="Введите название книги" onSearch={newValue} enterButton />
+        <Search placeholder="Введите Автора" onSearch={onSearch} enterButton />
+        <div>
+          <p>Выберите год публикации</p>
+          <Slider 
+          range defaultValue={[20, 50]}  
+          />
+        </div>
+        <Menu mode="inline">{getMenuItems(items)}</Menu>
       </div>
       <div className="bookColumn">
         {books.length > 0 ? (
